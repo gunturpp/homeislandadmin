d64ab4c00d577b8f7107a459e30129ac8b6cc73e
gunturpp <guntur_34@apps.ipb.ac.id>
2017-12-07 12:49:44 +0700

    user admin done

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,11 +1,11 @@
-{{--  @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">ADMIN Dashboard</div>
+                <div class="panel-heading">admins Dashboard</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,74 +14,10 @@
                         </div>
                     @endif
 
-                    You are logged in as <strong>ADMIN</strong>!
+                    You are logged in as <strong>admins</strong>!
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection  --}}
-
-@extends('layouts.app')
-
-@section('content')
-
-<div class="panel panel-info">
-	<div class="panel-heading">
-		<center>
-		<h1>
-		CRUD Laravel 5.3
-		</h1>
-		</center>
-	</div>
-	<div class="panel-body">
-		<a href="{{ URL('listuser/create') }}" class="btn btn-raised btn-primary pull-right">Tambah</a>
-		{{-- part alert --}}
-		
-			{{-- Kita cek, jika sessionnya ada maka tampilkan alertnya, jika tidak ada maka tidak ditampilkan alertnya --}}
-		
-		@if (Session::has('after_update'))
-			<div class="row">
-				<div class="col-md-12">
-					<div class="alert alert-dismissible alert-{{ Session::get('after_update.alert') }}">
-					  <button type="button" class="close" data-dismiss="alert">×</button>
-					  <strong>{{ Session::get('after_update.title') }}</strong>
-					  <a href="javascript:void(0)" class="alert-link">{{ Session::get('after_update.text-1') }}</a> {{ Session::get('after_update.text-2') }}
-					</div>
-				</div>
-			</div>
-		@endif
-		{{-- end part alert --}}
-		<table class="table table-bordered table-hover ">
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Nama Pengguna</th>
-					<th>Email</th>
-				</tr>
-			</thead>
-			<tbody>
-				@php(
-					$no = 1 {{-- buat nomor urut --}}
-					)
-				{{-- loop all kendaraan --}}
-				@foreach ($ListUser as $UserList)
-					<tr>
-						<td>{{ $no++ }}</td>
-						<td>{{ $UserList->nama_pengguna }}</td>
-						<td>{{ $UserList->email }}</td>
-						<td>
-							<center>
-								<a href="{{ URL('listuser/show') }}/{{ $UserList->id }}" class="btn btn-sm btn-raised btn-info">Edit</a>
-								<a href="{{ URL('listuser/destroy') }}/{{ $UserList->id }}" class="btn btn-sm btn-raised btn-danger">Hapus</a>
-							</center>
-						</td>
-					</tr>
-				@endforeach
-				{{-- // end loop --}}
-			</tbody>
-		</table>
-	</div>
-</div>
-
-@stop
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminController@index')->name('home');
+Route::get('/home', 'HomeController@index');
+
+Route::prefix('admin')->group(function(){
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+});
+
 Route::resource('cruds','CrudController');
